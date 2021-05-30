57d08cd49c21f7a47eb3bb38565921b3bd6797cf
lmdung <lmdung94@gmail.com>
2021-05-30 23:25:16 +0700

    day7

---
+++ day7/script.js
@@ -0,0 +1,10 @@
+const leftPage = document.querySelector('.left');
+const rightPage = document.querySelector('.right');
+const container = document.querySelector('.container');
+
+
+leftPage.addEventListener('mouseenter', () => container.classList.add('hover-left'));
+leftPage.addEventListener('mouseleave', () => container.classList.remove('hover-left'));
+
+rightPage.addEventListener('mouseenter', () => container.classList.add('hover-right'));
+rightPage.addEventListener('mouseleave', () => container.classList.remove('hover-right'));
