3fac968b93f2da7e2c5324867495e5d248edb6a9
lmdung <lmdung94@gmail.com>
2021-06-09 09:25:08 +0700

    day22

--- day22/script.js
+++ day22/script.js
@@ -2,7 +2,32 @@ const  canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 let size = 20;
-let color = 'black'
+let color = 'black';
+let pressed = false; // di chuot
+let x, y
+
+canvas.addEventListener('mousedown', (e) => {
+  pressed = true;
+  x = e.offsetX;
+  y = e.offsetY;
+  drawCircle(x, y)
+})
+canvas.addEventListener('mouseup', (e) => {
+  pressed = false;
+  x = undefined;
+  y = undefined;
+})
+canvas.addEventListener('mousemove', (e) => {
+  if (pressed) {
+    x2 = e.offsetX;
+    y2 = e.offsetY;
+    drawCircle (x2, y2)
+    drawLine(x, y, x2, y2)
+    
+    x = x2;
+    y = y2;
+  }
+})
 function drawCircle (x, y) {
   ctx.beginPath();
   ctx.arc(x, y, size, 0, Math.PI * 2, true);
@@ -14,6 +39,6 @@ function drawLine (x1, y1, x2, y2) {
     ctx.moveTo(x1, y1);
     ctx.lineTo(x2, y2);
     ctx.strokeStyle = color
-    ctx.lineWidth = size;
+    ctx.lineWidth = size*2;
     ctx.stroke();
 }
